ce739a10be6f39844f271132fe58af184811b923
Piotr Rutkowski <piorutko@gmail.com>
2020-10-04 16:11:45 +0200

    Updates

---
+++ precache-manifest.dfa0aec82d229483f49a33d916bbe6d0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff44b1f8e28171491c139129383db38d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9cbd1d423a2ac8cbaa9c",
+    "url": "/static/js/2.8e8b0917.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.8e8b0917.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0e2b9f84499ee7af883",
+    "url": "/static/js/main.175202b2.chunk.js"
+  },
+  {
+    "revision": "c923be74b2c97455ac4c",
+    "url": "/static/js/runtime-main.bfb323ef.js"
+  }
+]);
